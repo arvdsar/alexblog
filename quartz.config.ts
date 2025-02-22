945afa4e9e2932265b120ad0084dee3586f8ac61
Alexander <3330741+arvdsar@users.noreply.github.com>
2025-02-22 21:31:39 +0100

    Quartz sync: Feb 22, 2025, 9:31 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "new.vdsar.net",
+    baseUrl: "www.vdsar.net",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
